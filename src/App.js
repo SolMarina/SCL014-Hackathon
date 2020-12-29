17bd7eb0b22b116dee05e7cb23cbf8b6b43c00bf
Amanda D <adnamadiaz@gmail.com>
2020-12-29 19:36:38 -0300

    Create Repository

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Winner Team!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.somosmach.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Too MACH.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
