3cb7c5e820498590a5386aa312e53abc86522a93
SolMarina <sparraa17@gmail.com>
2021-01-04 22:35:59 -0300

    navbar

---
+++ src/Views/AllViews.js
@@ -0,0 +1,12 @@
+import React from "react";
+import View1 from './View1';
+
+const AllViews = () => {
+    return (
+        <div>
+            <View1 />
+        </div>
+
+    );
+};
+export default AllViews;
