d49538e5932025ee91658d1a53b9bdd28006fa28
Amanda D <adnamadiaz@gmail.com>
2021-01-06 11:03:21 -0300

    Add styles for NavBar componet

--- src/App.js
+++ src/App.js
@@ -7,25 +7,11 @@ import GanaView from './Views/GanaView';
 function App() {
   return (
     <div className="App">
+
+      <NavBar />
       
-        <NavBar />
-        <GanaView />
-        <p>
-          Winner Team!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.somosmach.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Too MACH.
-        </a>
-        <h1>Hola</h1>
-        <h2>Hola</h2>
-        <h3>Hola</h3>
-        <h4>Hola</h4>
-        
+      <GanaView />
+
       <BottomBar />
     </div>
   );
