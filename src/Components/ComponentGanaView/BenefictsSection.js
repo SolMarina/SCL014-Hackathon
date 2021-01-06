d0d04ecbfac05194e5c06fb8b7fd19502a4a18b0
acer <rverdufiguera@gmail.com>
2021-01-06 17:26:22 -0300

    cambios

--- src/Components/ComponentGanaView/BenefictsSection.js
+++ src/Components/ComponentGanaView/BenefictsSection.js
@@ -1,17 +1,24 @@
 import React from "react";
 import './BenefictsSection.css';
+import dataBenefits from '../../dataBenefits.json'; 
 
 const BenefictsSection = () => {
+
+
     return (
         <div>
             <h1>Beneficios</h1>
             <h3>Nivel 1</h3>
             <div>
-                Aqui los beneficios Nivel 1
+              
             </div>
             <h3>Nivel 2</h3>
             <div>
-                Aqui los beneficios Nivel 2
+            
+            </div>
+            <h3>Nivel 3</h3>
+            <div>
+          
             </div>
         </div>
 
