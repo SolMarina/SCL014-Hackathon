bbaa2a7d603136a00fb36cdfb7068ca52aa76780
SolMarina <sparraa17@gmail.com>
2021-01-05 22:39:34 -0300

    barra de progreso y referir a un amigo

--- src/Components/ComponentGanaView/ProgressBar.js
+++ src/Components/ComponentGanaView/ProgressBar.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './ProgressBar.css';
 
+
 const ProgressBar = () => {
+    const [progress, setProgress] = useState(25);
+
     return (
         <div>
-            <h1> Hola Hugo</h1>
-            <h2>Haz avanzado <span>32 pts</span> al siguiente nivel.</h2>
-            <div>
-                Aqui va la barra de progreso
-            </div>
+            <div className="barProgress">
+                <h1> Hola Hugo</h1>
+                <h2>Haz avanzado <span>32 pts</span> al siguiente nivel.</h2>
+                <div>
+                    <div className="progress">
+                        <div className="progress-bar0 progress-bar-striped progress-bar-animated" style={{ width: progress + '%' }} role="progressbar" aria-valuenow="1" aria-valuemin="2" aria-valuemax="100">1</div>
+                        <div className="progress2"> 2</div>
+                    </div>
 
+                </div>
+                <button onClick={() => setProgress(progress + 10)}>Ruleta</button>
+                <div className="avance">
+                    <div>Faltan 18 puntos</div>
+                    
+                </div>
+            </div>
+            <br></br>
         </div>
+
 
     );
 };
