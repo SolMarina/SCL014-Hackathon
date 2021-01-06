94ba73c7785f36abb89daa0a52c2cd6fe6a78891
Amanda D <adnamadiaz@gmail.com>
2021-01-05 22:34:17 -0300

    Add new Components RouletteSection

---
+++ src/Components/ComponentGanaView/Ruleta.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ruleta from './rueda.png'
+// import './index.css';
+
+const Ruleta = (props) => (
+
+    <div>
+        <h1 align="center" className="ruleta-puntos">Puntos: {props.total_points}</h1>
+        <p align="center">
+            <img
+                id="img-ruleta"
+                src={ruleta}
+                style={{ transform: 'rotate(' + props.data_ruleta + 'deg)', WebkitTransform: 'rotate(' + props.data_ruleta + 'deg)' }}
+                alt="Ruleta"
+                onTransitionEnd={props.showRuletaResult}
+                className="img-responsive img-ruleta"
+                ref={props.ruleta}
+            />
+        </p>
+        <p align="center">
+            <button id="btnAnimar" disabled={props.animatedRuleta} onClick={props.animarEvent} className="btn btn-warning btn-orange btn-lg">GIRA LA RULETA</button>
+        </p>
+    </div>
+
+)
+
+export default Ruleta
